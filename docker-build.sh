dac2c2b59843dd366436f645f61dcbc331387bda
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-05-25 10:49:45 +0200

    update Docker build processes

--- docker-build.sh
+++ docker-build.sh
@@ -7,3 +7,8 @@ echo
 echo ${cyn}Building Docker image ...${end}
 docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION .
 echo ${cyn}Docker image built${end}
+echo
+echo ${cyn}Pushing Docker image to Docker Hub...${end}
+docker tag $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION $DOCKER_HUB_ID/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
+docker push $DOCKER_HUB_ID/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION
+echo ${cyn}Docker image pushed${end}
