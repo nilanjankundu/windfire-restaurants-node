34db2021f864b4e17c804840319503f39e98a5b9
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-10-03 15:20:56 +0200

    updates

--- deployment/openshift/deploy.sh
+++ deployment/openshift/deploy.sh
@@ -1,12 +1,22 @@
 # ##### START - Variable section
 RUN_FUNCTION=
+OPENSHIFT_PROJECT=
+APP_LABEL=
 # ##### END - Variable section
 
 # ***** START - Function section
 deploy()
 {
     echo "***************** TODO *****************"
-    oc new-app --name windfire-restaurants-backend https://github.com/robipozzi/windfire-restaurants-node -o yaml
+    OPENSHIFT_PROJECT=windfire
+    APP_LABEL=windfire-restaurants-backend
+    oc new-project $OPENSHIFT_PROJECT
+    oc project $OPENSHIFT_PROJECT
+    #oc new-app --name $APP_LABEL https://github.com/robipozzi/windfire-restaurants-node --context-dir=app -o yaml > template.yaml
+    oc new-app --name $APP_LABEL https://github.com/robipozzi/windfire-restaurants-node --context-dir=app
+    oc patch svc windfire-restaurants-backend --type=json -p '[{"op": "replace", "path": "/spec/ports/0/targetPort", "value":8082}]'
+    oc expose svc $APP_LABEL
+    oc get route
 }
 # ***** END - Function section
 
--- deployment/openshift/undeploy.sh
+++ deployment/openshift/undeploy.sh
@@ -1,11 +1,17 @@
 # ##### START - Variable section
 RUN_FUNCTION=
+OPENSHIFT_PROJECT=
+APP_LABEL=
 # ##### END - Variable section
 
 # ***** START - Function section
 undeploy()
 {
     echo "***************** TODO *****************"
+    OPENSHIFT_PROJECT=windfire
+    APP_LABEL=windfire-restaurants-backend
+    oc project $OPENSHIFT_PROJECT
+    oc delete all -l app=$APP_LABEL
 }
 # ***** END - Function section
 
