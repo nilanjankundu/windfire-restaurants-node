be842d5f663417b421192c57968db8ea2211894f
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-10-03 15:33:52 +0200

    update

--- deployment/openshift/deploy.sh
+++ deployment/openshift/deploy.sh
@@ -12,7 +12,6 @@ deploy()
     APP_LABEL=windfire-restaurants-backend
     oc new-project $OPENSHIFT_PROJECT
     oc project $OPENSHIFT_PROJECT
-    #oc new-app --name $APP_LABEL https://github.com/robipozzi/windfire-restaurants-node --context-dir=app -o yaml > template.yaml
     oc new-app --name $APP_LABEL https://github.com/robipozzi/windfire-restaurants-node --context-dir=app
     oc patch svc windfire-restaurants-backend --type=json -p '[{"op": "replace", "path": "/spec/ports/0/targetPort", "value":8082}]'
     oc expose svc $APP_LABEL
