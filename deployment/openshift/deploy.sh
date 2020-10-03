fd4a47ca1dfcdd415bee604b09cac76bed386112
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-10-03 15:40:17 +0200

    update

--- deployment/openshift/deploy.sh
+++ deployment/openshift/deploy.sh
@@ -1,3 +1,5 @@
+source setenv.sh
+
 # ##### START - Variable section
 RUN_FUNCTION=
 OPENSHIFT_PROJECT=
@@ -7,14 +9,15 @@ APP_LABEL=
 # ***** START - Function section
 deploy()
 {
-    echo "***************** TODO *****************"
+    echo ${red}***************** TODO *****************${end}
     OPENSHIFT_PROJECT=windfire
     APP_LABEL=windfire-restaurants-backend
     oc new-project $OPENSHIFT_PROJECT
     oc project $OPENSHIFT_PROJECT
-    oc new-app --name $APP_LABEL https://github.com/robipozzi/windfire-restaurants-node --context-dir=app
-    oc patch svc windfire-restaurants-backend --type=json -p '[{"op": "replace", "path": "/spec/ports/0/targetPort", "value":8082}]'
-    oc expose svc $APP_LABEL
+    oc new-app -f $PWD/deployment/openshift/template.yaml
+    #oc new-app --name $APP_LABEL https://github.com/robipozzi/windfire-restaurants-node --context-dir=app
+    #oc patch svc windfire-restaurants-backend --type=json -p '[{"op": "replace", "path": "/spec/ports/0/targetPort", "value":8082}]'
+    #oc expose svc $APP_LABEL
     oc get route
 }
 # ***** END - Function section
