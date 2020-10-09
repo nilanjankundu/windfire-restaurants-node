8ec26630f3d6ca7176f0f14805e79ea975ee2120
Roberto Pozzi <robertopozzi@Robertos-MBP.lan>
2020-10-09 16:07:33 +0200

    update

--- deployment/openshift/deploy.sh
+++ deployment/openshift/deploy.sh
@@ -9,7 +9,7 @@ deploy()
 {
     oc new-project $OPENSHIFT_PROJECT
     oc project $OPENSHIFT_PROJECT
-    oc new-app -f $PWD/deployment/openshift/windfire-restaurants-backend-template.yaml
+    oc new-app -f $PWD/deployment/openshift/windfire-restaurants-backend-template.yaml -p GITHUB_BRANCH=dev
     #oc patch svc OPENSHIFT_APP_LABEL --type=json -p '[{"op": "replace", "path": "/spec/ports/0/targetPort", "value":8082}]'
     ROUTE_URL=$(oc get route windfire-restaurants-backend -o jsonpath='{.spec.host}')
     echo Test it at ${grn}$ROUTE_URL${end}
